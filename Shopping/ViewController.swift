087148bf890dffe59d03156f8beee8c7414ccdf0
Ed McCormic <edmccormic@gmail.com>
2015-08-11 15:51:01 -0600

    Initial Commit

---
+++ Shopping/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shopping
+//
+//  Created by ED on 8/11/15.
+//  Copyright (c) 2015 SwiftBeard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
